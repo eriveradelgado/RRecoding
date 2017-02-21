23b474ab5e1abdcd2c5f83a8b5d149efd1e71464
eriveradelgado <edgardo.rivera@case.edu>
2017-02-21 17:22:21 +0000

    delete guest cleaning step, reformat, convert host from symbol

--- 02-DataCleaning.R
+++ 02-DataCleaning.R
@@ -1,50 +1,63 @@
 #I did reformatting throughout the entire code
 #Trouble shooting will be indicated by comments -AX
 
-require(dplyr)
+require(tidyverse)
 require(stringr)
-require(tidyr)
-require(stringr)
+# require(tidyr)
+# require(stringr)
 ri_bound <- readRDS(file = "./Output Data/01-ri_boundDF.rds")
+
+
+# Rename columns 
 
 ri_bound <- ri_bound %>%
   rename(DelG = `ΔG°/ kJ mol-1`, 
-       DelH =`ΔH°/ kJ mol-1`,
-       TDelS = `TΔS°/ kJ mol-1`,
-       DelCp = `ΔCp°/ J mol-1 K-1`,
-       log.K = `log K`)
+         DelH =`ΔH°/ kJ mol-1`,
+         TDelS = `TΔS°/ kJ mol-1`,
+         DelCp = `ΔCp°/ J mol-1 K-1`,
+         log.K = `log K`,
+         method = `methoda`)
 
 
 # Splitting columns containing a variable value and its uncertainty
-#Fixed the separator -AX
+
 ri_clean <- ri_bound %>% 
-        separate(solvent, c("solvent","solvent.specs"),
-                 sep = "(?=\\s*\\()", extra = "merge", fill = "right") %>% 
-        separate(log.K, c("log.K", "log.K.Uncertainty"), 
-                 sep = "\\s\\±\\s", extra = "merge", fill = "right") %>% 
-        separate(DelG, c("DelG", "DelG.Uncertainty"),
-                 sep = "\\s\\±\\s",extra = "merge", fill = "right") %>%
-        separate(DelH, c("DelH", "DelH.Uncertainty"),
-                 sep = "\\s\\±\\s", extra = "merge", fill = "right") %>%
-        separate(TDelS, c("TDelS", "TDelS.Uncertainty"), 
-                 sep = "\\s\\±\\s",extra = "merge", fill = "right") %>%
-        separate(DelCp, c("DelCp", "DelCp.Uncertainty"), 
-                 sep = "\\s\\±\\s",extra = "merge", fill = "right") %>%
-         lapply(str_replace_all, pattern = "\\−", replacement = "-") %>%
-         lapply(str_replace_all, pattern = "\\ +", replacement = " ") %>%
-         lapply(str_replace_all, pattern = "\\s+", replacement = " ") %>%
-         lapply(str_replace_all, pattern = "·", replacement = " ") %>%
-         as_tibble()
+  separate(solvent, c("solvent","solvent.specs"),
+           sep = "(?=\\s*\\()", extra = "merge", fill = "right") %>% 
+  separate(log.K, c("log.K", "log.K.Uncertainty"), 
+           sep = "\\s\\±\\s", extra = "merge", fill = "right") %>% 
+  separate(DelG, c("DelG", "DelG.Uncertainty"),
+           sep = "\\s\\±\\s",extra = "merge", fill = "right") %>%
+  separate(DelH, c("DelH", "DelH.Uncertainty"),
+           sep = "\\s\\±\\s", extra = "merge", fill = "right") %>%
+  separate(TDelS, c("TDelS", "TDelS.Uncertainty"), 
+           sep = "\\s\\±\\s",extra = "merge", fill = "right") %>%
+  separate(DelCp, c("DelCp", "DelCp.Uncertainty"), 
+           sep = "\\s\\±\\s",extra = "merge", fill = "right")
+
+# Cleaning strings that contain inconsistent patterns such as more than one space
+# separation and unconventional separation symbols. Converting alpha, beta and
+# gamma symbols to words for easier subset of tables 
+
+ri_clean <- ri_clean %>%
+  lapply(str_replace_all, pattern = "\\−", replacement = "-") %>%
+  lapply(str_replace_all, pattern = "\\ +", replacement = " ") %>%
+  lapply(str_replace_all, pattern = "\\s+", replacement = " ") %>%
+  lapply(str_replace_all, pattern = "·", replacement = " ") %>% 
+  as_tibble() %>%
+  mutate(host = str_replace(host, pattern =  "\\α",replacement = "alpha" )) %>%
+  mutate(host = str_replace(host, pattern = "\\β|\\B\\s+H\\+", replacement = "beta")) %>%
+  mutate(host = str_replace(host, pattern = "\\γ", replacement = "gamma"))
          
       
                        
 
 
 #=============================================================================== 
-#                 "Variable Engineering or Imputation"                         =
+#                             pH Imputation                                    =
 #===============================================================================
-# Remove pH string and \\) to convert the pH Column to numerical. Assume that 
-# columns with no value have a pH of 7.0. pH values with a range of 
+# Remove pH string and parenthesis to convert the pH Column to numerical. Assume 
+# that columns with no value have a pH of 7.0. pH values with a range of 
 # phValue1-phValue2 will be assigned an average pH value ((phValue1+phValue2)/2)
 # ph's with an inequality (<,>) will be set at the value given. Guests with 
 # the molarity of the acid given in the solvent composition will be set
@@ -59,10 +72,14 @@ pH.numeric <-  "[0-9]+\\.*[0-9]*"
 ri_clean <- ri_clean %>% 
   mutate(ref.notes= str_extract(ref, pattern = "[:alpha:]"),
          ref = str_extract(ref, pattern = "\\d+(\\,\\s\\d+)*"),
-         pH = str_extract(solvent.specs, pattern = "(pH\\s(\\<\\s)*[0-9]+(\\.[0-9]+)*)"),
-         pH.range = str_extract(solvent.specs, pattern = "pH\\s[0-9]+\\.[0-9]+\\-[0-9]+(\\.[0-9]+)*"),
-         ratio = str_extract(solvent.specs, pattern = "[0-9]+(\\.[0-9]+)*\\:[0-9]+(\\.[0-9]+)*"),
-         solvent.molarity = str_extract(solvent.specs, pattern = pattern.molarity)) %>%
+         pH = str_extract(solvent.specs, 
+                          pattern = "(pH\\s(\\<\\s)*[0-9]+(\\.[0-9]+)*)"),
+         pH.range = str_extract(solvent.specs,
+                                pattern = "pH\\s[0-9]+\\.[0-9]+\\-[0-9]+(\\.[0-9]+)*"),
+         solvent.ratio = str_extract(solvent.specs,
+                                     pattern = "[0-9]+(\\.[0-9]+)*\\:[0-9]+(\\.[0-9]+)*"),
+         solvent.molarity = str_extract(solvent.specs, 
+                                        pattern = pattern.molarity)) %>%
   mutate(pH = ifelse(!is.na(pH.range), NA, pH)) %>%
   mutate(pH = str_extract(pH, pattern = pH.numeric)%>% as.numeric())%>%
   separate(., pH.range, c("pH1", "pH2"), sep = "-") %>%
@@ -74,14 +91,8 @@ ri_clean <- ri_clean %>%
 
 
 
-# pattern 2 and pattern 3 extract the numeric ph values from solvent composition 
-#the \u2212 is the unicode for a "minus" sign, different from the keyboard default hyphen -AX
-#putting a regular dash in the code cuts off some entries -AX
-#Also summarized the regex a bit -AX
-
-
-
 # Setting pH to acidic value when the solution contained an acid
+
 sulfuric_acid <- grep(pattern = "[M]+\\s+\\bH2SO4\\b", 
                       x = ri_clean$solvent.specs)
 
@@ -91,97 +102,71 @@ hcl_acid <- grep(pattern = "M+\\s+\\bHCl\\b",
 ri_clean$pH[sulfuric_acid] <- 1
 ri_clean$pH[hcl_acid]  <-  1.21
 
+# ------------------------------------------------------------------------------
+#                          Reconsider this Section
+# ------------------------------------------------------------------------------
+# It would be nice to explain what each of these cleanup step is actually doing
+# to the guest column and why is needed. I did a quick search in google 
+# and was able to find many   of the structures without modifying the original 
+# name. Maybe a guest.special.cases column could be more useful - ERD
+# 
+# ri_engineered <- ri_clean %>%
+#   mutate(clean.guest = str_replace_all(
+#     string = guest,
+#     pattern = "\\·+\\s*[A-Z]*[a-z]*",
+#     replacement = ""
+#   )) %>% 
+#   mutate(
+#     clean.guest = str_replace_all(
+#       string = clean.guest,
+#       pattern = "[0-9]HCl",
+#       replacement = "hydrochloride"
+#     )
+#   ) %>%
+#   mutate(clean.guest = str_replace_all(
+#     string = clean.guest,
+#     pattern = "\\β",
+#     replacement = "beta"
+#   )) %>%
+#   mutate(clean.guest = str_replace(
+#     string = clean.guest,
+#     pattern = "\\α",
+#     replacement = "alpha"
+#   )) %>%
+# 
+#   mutate(clean.guest = str_replace(
+#     string = clean.guest,
+#     pattern = "HCl",
+#     replacement = "hydrochloride"
+#   )) %>%
+#   mutate(clean.guest = str_replace(
+#     string = clean.guest,
+#     pattern = "H2SO4",
+#     replacement = "sulfonate"
+#   )) %>% select(guest, clean.guest) %>% unique() %>% View()
+#   tbl_df() %>%
+#   bind_cols(ri_clean) %>%
+#   
+# 
+# #fixed file path -AX
+# clean_dir       <- "./Output Data/"
+# saveRDS(ri_engineered, file = paste0(clean_dir, "02-ri_engineered.RDS"))
+# save(ri_engineered, file = paste0(clean_dir, "ri_engineered.RData"))
+# ------------------------------------------------------------------------------
 
-# It could be nice to explain what each of these cleanup step is actually doing
-# to guest. I did a quick search in google and was able to find many of the
-# structures without modifying the original name
-ri_engineered <- ri_clean %>%
-  mutate(clean.guest = str_replace_all(
-    string = guest,
-    pattern = "\\·+\\s*[A-Z]*[a-z]*",
-    replacement = ""
-  )) %>% 
-  # transmute(
-  #   clean.guest = str_replace_all(
-  #     string = clean.guest,
-  #     pattern = "\\([di]*[mono]*anion\\)",
-  #     replacement = ""
-  #   )
-  # ) %>%
-  transmute(
-    clean.guest = str_replace_all(
-      string = clean.guest,
-      pattern = "\\s*derivative\\s*(\\(E*Z*\\))*(cis)?(trans)?\\-*[0-9]*",
-      replacement = ""
-    )
-  ) %>%select(guest, clean.guest) %>% unique() %>% View()
-  transmute(
-    clean.guest = str_replace_all(
-      string = clean.guest,
-      pattern = "[0-9]HCl",
-      replacement = "hydrochloride"
-    )
-  ) %>%
-  # transmute(clean.guest = str_replace_all(
-  #   string = clean.guest,
-  #   pattern = gsub(
-  #     pattern = "\\n",
-  #     replacement = "",
-  #     x = "\\(\\-*\\±*[0-9A-Z]*\\,*[0-9A-Z]*\\)\\-\\(*\\+*\\-*\\)*\\-*|nor(?!t)|\\([a-z]*\\,*\\s*[a-z]*(I[0-9]\\-)*\\)"
-  #   ),
-  #   replacement = ""
-  # )) %>%
-  transmute(clean.guest = str_replace_all(
-    string = clean.guest,
-    pattern = "\\β",
-    replacement = "beta"
-  )) %>%
-  transmute(clean.guest = str_replace(
-    string = clean.guest,
-    pattern = "\\α",
-    replacement = "alpha"
-  )) %>%
-  # transmute(
-  #   clean.guest = str_replace(
-  #     string = clean.guest,
-  #     pattern = "cis\\-|trans\\-|\\((?=\\[)",
-  #     replacement = ""
-  #   )
-  # ) %>%
-  transmute(clean.guest = str_replace(
-    string = clean.guest,
-    pattern = "HCl",
-    replacement = "hydrochloride"
-  )) %>%
-  transmute(clean.guest = str_replace(
-    string = clean.guest,
-    pattern = "H2SO4",
-    replacement = "sulfonate"
-  )) %>%
-  # transmute(
-  #   clean.guest = str_replace(
-  #     string = clean.guest,
-  #     pattern = "\\s[0-9]+[a-z]$|\\s[A-Z][0-9]",
-  #     replacement = ""
-  #   )
-  # ) %>%
-  tbl_df() %>%
-  bind_cols(ri_clean) %>%
-  select(2:3, 1, everything())
 
-#fixed file path -AX
-clean_dir       <- "~/SREP LAB/R Recoding/Cleaned Data/"
-saveRDS(ri_engineered, file = paste0(clean_dir, "ri_engineered.RDS"))
-save(ri_engineered, file = paste0(clean_dir, "ri_engineered.RData"))
-                
-                
+# ------------------------------------------------------------------------------
+#                             Formatting Table           
+# Reorganizing Table  into the following colunn families vector types
+# Host <chr> | Guest <chr> | Solvent Specs <chr>| pH + Thermodynamic Values (dbl)
+# | Method+References <chr>
+ri_clean <- ri_clean %>%
+  select(1:4, 20:21, pH, 5:13, DelCp, DelCp.Uncertainty, method, ref, ref.notes) %>%
+  mutate_at(vars(7:18), as.numeric) 
         
 
-ri_clean[ , 6:15] <- ri_clean%>%
-        select(pH:TDelS.Uncertainty) %>%
-        lapply(destring)
+# -----------------------------------------------------------------------------
+#                     Save output of script 
 
-#SREP LAB is my own personal file
-
-saveRDS(object = ri_clean, file = "./Output Data/02-ri_clean.rds")
+saveRDS(ri_clean, file = "./Output Data/02-ri_clean.rds")
 save(ri_clean, file = "./Output Data/02-ri_clean.RData")
